c4b885279d8baa4d1cfd2d25b88e6ed16824663d
barrust <barrust@gmail.com>
2022-10-29 23:04:28 -0400

    move to pyproject.toml for build

--- omdb/omdb.py
+++ omdb/omdb.py
@@ -5,15 +5,15 @@ import requests
 
 from .exceptions import (
     OMDBException,
-    OMDBNoResults,
-    OMDBLimitReached,
-    OMDBTooManyResults,
     OMDBInvalidAPIKey,
+    OMDBLimitReached,
+    OMDBNoResults,
+    OMDBTooManyResults,
 )
 from .utilities import camelcase_to_snake_case, range_inclusive, to_int
 
 
-class OMDB(object):
+class OMDB:
     """ The OMDB API wrapper instance
 
         Args:
@@ -29,7 +29,7 @@ class OMDB(object):
     __slots__ = ["_api_url", "_timeout", "_api_key", "_session", "_strict"]
 
     def __init__(self, api_key, timeout=5, strict=True):
-        """ the init object """
+        """the init object"""
         self._api_url = "https://www.omdbapi.com/"
         self._timeout = None
         self.timeout = timeout
@@ -40,19 +40,19 @@ class OMDB(object):
         self._session = requests.Session()
 
     def close(self):
-        """ Close the requests connection if necessary """
+        """Close the requests connection if necessary"""
         if self._session:
             self._session.close()
             self._session = None
 
     @property
     def api_key(self):
-        """ str: The API Key to use to connect to the OMDB API """
+        """str: The API Key to use to connect to the OMDB API"""
         return self._api_key
 
     @api_key.setter
     def api_key(self, val):
-        """ set the API Key """
+        """set the API Key"""
         if isinstance(val, str):
             self._api_key = val
         else:
@@ -60,42 +60,39 @@ class OMDB(object):
 
     @property
     def timeout(self):
-        """ float: The timeout parameter to pass to requests for how long to wait """
+        """float: The timeout parameter to pass to requests for how long to wait"""
         return self._timeout
 
     @timeout.setter
     def timeout(self, val):
-        """ set the timeout property """
+        """set the timeout property"""
         try:
             self._timeout = float(val)
-        except ValueError:
-            msg = "OMDB Timeout must be a float or convertable to float! {} provided".format(
-                val
-            )
-            raise ValueError(msg)
+        except ValueError as exc:
+            raise ValueError(f"OMDB Timeout must be a float or convertable to float! {val} provided") from exc
 
     @property
     def strict(self):
-        """ bool: Whether to throw or swallow errors; True will throw exceptions """
+        """bool: Whether to throw or swallow errors; True will throw exceptions"""
         return self._strict
 
     @strict.setter
     def strict(self, val):
-        """ set the strict property """
+        """set the strict property"""
         self._strict = bool(val)
 
     def search(self, title, pull_all_results=True, page=1, **kwargs):
-        """ Perform a search based on title
+        """Perform a search based on title
 
-            Args:
-                title (str): The query string to lookup
-                page (int): The page of results to return
-                pull_all_results (bool): True to return all results; False to pull page only
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Note:
-                If `pull_all_results` is `True` then page is ignored """
+        Args:
+            title (str): The query string to lookup
+            page (int): The page of results to return
+            pull_all_results (bool): True to return all results; False to pull page only
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Note:
+            If `pull_all_results` is `True` then page is ignored"""
         params = {
             "s": title,
             "page": 1,
@@ -103,44 +100,40 @@ class OMDB(object):
         }  # set to the default...
 
         if not pull_all_results:
-            params[
-                "page"
-            ] = page  # we are going to set it so that we can pull everything!
+            params["page"] = page  # we are going to set it so that we can pull everything!
 
         params.update(kwargs)
 
         results = self._get_response(params)
 
         total_results = int(results.get("total_results", 0))
-        if (
-            not pull_all_results or total_results <= 10
-        ):  # 10 is the max that it will ever return
+        if not pull_all_results or total_results <= 10:  # 10 is the max that it will ever return
             return results
 
         if "search" not in results:
-            results["search"] = list()  # defensive
+            results["search"] = []  # defensive
 
         max_i = ceil(total_results / 10)
         for i in range_inclusive(2, max_i):
             params.update({"page": i})
             data = self._get_response(params)
-            results["search"].extend(data.get("search", list()))
+            results["search"].extend(data.get("search", []))
 
         return results
 
     def get(self, *, title=None, imdbid=None, **kwargs):
-        """ Retrieve a specific movie, series, or episode
+        """Retrieve a specific movie, series, or episode
 
-            Args:
-                title (str): The title of the movie, series, or episode to return
-                imdbid (str): The IMDB Id to use to pull the result
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Raises:
-                OMDBException: Raised when both title or imdbid is not provided
-            Note:
-                Either `title` or `imdbid` is required """
+        Args:
+            title (str): The title of the movie, series, or episode to return
+            imdbid (str): The IMDB Id to use to pull the result
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Raises:
+            OMDBException: Raised when both title or imdbid is not provided
+        Note:
+            Either `title` or `imdbid` is required"""
         params = {"apikey": self.api_key}
         if imdbid:
             params["i"] = imdbid
@@ -154,67 +147,67 @@ class OMDB(object):
         return self._get_response(params)
 
     def search_movie(self, title, pull_all_results=True, page=1, **kwargs):
-        """ Search for a movie by title
+        """Search for a movie by title
 
-            Args:
-                title (str): The name, or part of a name, of the movie to look up
-                pull_all_results (bool): True to return all results; False to pull page only
-                page (int): The page of results to return
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results """
+        Args:
+            title (str): The name, or part of a name, of the movie to look up
+            pull_all_results (bool): True to return all results; False to pull page only
+            page (int): The page of results to return
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results"""
         params = {"type": "movie"}
         params.update(kwargs)
         return self.search(title, pull_all_results, page, **params)
 
     def search_series(self, title, pull_all_results=True, page=1, **kwargs):
-        """ Search for a TV series by title
+        """Search for a TV series by title
 
-            Args:
-                title (str): The name, or part of a name, of the TV series to look up
-                pull_all_results (bool): True to return all results; False to pull page only
-                page (int): The page of results to return
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results """
+        Args:
+            title (str): The name, or part of a name, of the TV series to look up
+            pull_all_results (bool): True to return all results; False to pull page only
+            page (int): The page of results to return
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results"""
         params = {"type": "series"}
         params.update(kwargs)
         return self.search(title, pull_all_results, page, **params)
 
     def get_movie(self, *, title=None, imdbid=None, **kwargs):
-        """ Retrieve a movie by title or IMDB id
+        """Retrieve a movie by title or IMDB id
 
-            Args:
-                title (str): The name of the movie to retrieve
-                imdbid (str): The IMDB id of the movie to retrieve
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Note:
-                Either `title` or `imdbid` is required """
+        Args:
+            title (str): The name of the movie to retrieve
+            imdbid (str): The IMDB id of the movie to retrieve
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Note:
+            Either `title` or `imdbid` is required"""
         params = {"type": "movie"}
         params.update(kwargs)
         return self.get(title=title, imdbid=imdbid, **params)
 
     def get_series(self, *, title=None, imdbid=None, pull_episodes=False, **kwargs):
-        """ Retrieve a TV series information by title or IMDB id
+        """Retrieve a TV series information by title or IMDB id
 
-            Args:
-                title (str): The name of the TV series to retrieve
-                imdbid (str): The IMDB id of the TV series to retrieve
-                pull_episodes (bool): `True` to pull the episodes
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Note:
-                Either `title` or `imdbid` is required """
+        Args:
+            title (str): The name of the TV series to retrieve
+            imdbid (str): The IMDB id of the TV series to retrieve
+            pull_episodes (bool): `True` to pull the episodes
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Note:
+            Either `title` or `imdbid` is required"""
         params = {"type": "series"}
         params.update(kwargs)
         res = self.get(title=title, imdbid=imdbid, **params)
         num_seasons = 0
         if pull_episodes:
             num_seasons = to_int(res.get("total_seasons", 0))
-            res["seasons"] = dict()
+            res["seasons"] = {}
 
         for i in range(num_seasons):
             season_num = i + 1
@@ -224,18 +217,18 @@ class OMDB(object):
         return res
 
     def get_episode(self, *, title=None, imdbid=None, season=1, episode=1, **kwargs):
-        """ Retrieve a TV series episode by title or IMDB id and season and episode number
+        """Retrieve a TV series episode by title or IMDB id and season and episode number
 
-            Args:
-                title (str): The name of the TV series to retrieve
-                imdbid (str): The IMDB id of the TV series to retrieve
-                season (int): The season number of the episode to retrieve
-                episode (int): The episode number (based on season) of the episode to retrieve
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Note:
-                Either `title` or `imdbid` is required """
+        Args:
+            title (str): The name of the TV series to retrieve
+            imdbid (str): The IMDB id of the TV series to retrieve
+            season (int): The season number of the episode to retrieve
+            episode (int): The episode number (based on season) of the episode to retrieve
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Note:
+            Either `title` or `imdbid` is required"""
         params = {"type": "episode"}
         if season:
             params["Season"] = season
@@ -245,40 +238,36 @@ class OMDB(object):
         return self.get(title=title, imdbid=imdbid, **params)
 
     def get_episodes(self, *, title=None, imdbid=None, season=1, **kwargs):
-        """ Retrieve all episodes of a TV series by season number
+        """Retrieve all episodes of a TV series by season number
 
-            Args:
-                title (str): The name of the TV series to retrieve
-                imdbid (str): The IMDB id of the movie to retrieve
-                season (int): The season number of the episode to retrieve
-                kwargs (dict): the kwargs to add additional parameters to the API request
-            Returns:
-                dict: A dictionary of all the results
-            Note:
-                Either `title` or `imdbid` is required """
-        return self.get_episode(
-            title=title, imdbid=imdbid, season=season, episode=None, **kwargs
-        )
+        Args:
+            title (str): The name of the TV series to retrieve
+            imdbid (str): The IMDB id of the movie to retrieve
+            season (int): The season number of the episode to retrieve
+            kwargs (dict): the kwargs to add additional parameters to the API request
+        Returns:
+            dict: A dictionary of all the results
+        Note:
+            Either `title` or `imdbid` is required"""
+        return self.get_episode(title=title, imdbid=imdbid, season=season, episode=None, **kwargs)
 
     def _get_response(self, kwargs):
-        """ wrapper for the `requests` library call """
-        response = self._session.get(
-            self._api_url, params=kwargs, timeout=self._timeout
-        ).json()
+        """wrapper for the `requests` library call"""
+        response = self._session.get(self._api_url, params=kwargs, timeout=self._timeout).json()
         return self.__format_results(response, kwargs)
 
     def __format_results(self, res, params):
-        """ format the results into non-camelcase dictionaries """
+        """format the results into non-camelcase dictionaries"""
         if not isinstance(res, dict):
-            raise TypeError("Expecting dict type, recieved {}".format(type(res)))
+            raise TypeError(f"Expecting dict type, recieved {type(res)}")
 
-        keys = sorted([x for x in res.keys()])
+        keys = sorted(list(res.keys()))
         for key in keys:
             val = res.pop(key)
             if isinstance(val, dict):
                 val = self.__format_results(val, params)
             if isinstance(val, list):
-                tmp = list()
+                tmp = []
                 for _, itm in enumerate(val):
                     if isinstance(itm, dict):
                         tmp.append(self.__format_results(itm, params))
@@ -294,12 +283,12 @@ class OMDB(object):
             err = res.get("error", "").lower()
             if err == "too many results.":
                 raise OMDBTooManyResults(res["error"], params)
-            elif err in ("movie not found!", "series or season not found!"):
+            if err in ("movie not found!", "series or season not found!"):
                 raise OMDBNoResults(res["error"], params)
-            elif err == "request limit reached!":
+            if err == "request limit reached!":
                 raise OMDBLimitReached(self.api_key)
-            elif err == "invalid api key!":
+            if err == "invalid api key!":
                 raise OMDBInvalidAPIKey(self.api_key)
-            raise OMDBException("An unknown exception was returned: {}".format(err))
+            raise OMDBException(f"An unknown exception was returned: {err}")
 
         return res
--- omdb/utilities.py
+++ omdb/utilities.py
@@ -2,10 +2,10 @@
 
 
 def camelcase_to_snake_case(_input):
-    """ Convert a camel case string to a snake case string: CamelCase -> camel_case
+    """Convert a camel case string to a snake case string: CamelCase -> camel_case
 
-        Args:
-            _input (str): The string to convert """
+    Args:
+        _input (str): The string to convert"""
     # https://codereview.stackexchange.com/a/185974
     res = _input[0].lower()
     for i, letter in enumerate(_input[1:], 1):
@@ -20,26 +20,25 @@ def camelcase_to_snake_case(_input):
 
 
 def range_inclusive(start, end, step=1):
-    """ Return the range of elements inclusive of the end value
+    """Return the range of elements inclusive of the end value
 
-        Args:
-            start (int): The start value of the range
-            end (int): The end value of the range
-            step (int): The step value to use
-        Yields:
-            int: The current value within the range
+    Args:
+        start (int): The start value of the range
+        end (int): The end value of the range
+        step (int): The step value to use
+    Yields:
+        int: The current value within the range
     """
-    for i in range(start, end + 1, step):
-        yield i
+    yield from range(start, end + 1, step)
 
 
 def to_int(val):
-    """ Turn the passed in variable into an int; returns 0 if errors
+    """Turn the passed in variable into an int; returns 0 if errors
 
-        Args:
-            val (str): The variable to turn into an int
-        Returns:
-            int: The int value if possible, 0 if an error occurs
+    Args:
+        val (str): The variable to turn into an int
+    Returns:
+        int: The int value if possible, 0 if an error occurs
     """
     try:
         return int(val)
