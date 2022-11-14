ae79b284f551907649540c448d4d457306d06be9
Tyler Barrus <barrust@gmail.com>
2022-11-14 17:21:40 -0500

    Feature/build upgrade (#10)
    * move to pyproject.toml for build
    
    * update supported versions

--- omdb/__init__.py
+++ omdb/__init__.py
@@ -1,12 +1,12 @@
 """ the omdb module """
 
-from .omdb import OMDB
 from .exceptions import (
     OMDBException,
-    OMDBNoResults,
     OMDBLimitReached,
+    OMDBNoResults,
     OMDBTooManyResults,
 )
+from .omdb import OMDB
 
 __author__ = "Tyler Barrus"
 __maintainer__ = "Tyler Barrus"
@@ -14,7 +14,7 @@ __email__ = "barrust@gmail.com"
 __license__ = "MIT"
 __version__ = "0.2.1"
 __url__ = "https://github.com/barrust/pyomdbapi"
-__bugtrack_url__ = "{0}/issues".format(__url__)
+__bugtrack_url__ = f"{__url__}/issues"
 __all__ = [
     "OMDB",
     "OMDBException",
